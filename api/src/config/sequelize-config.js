8c1df5cfd5e92bf54085023f7a7c3b4edcffa848
mhd-zaid <zaidmouhamad@gmail.com>
2023-09-21 12:14:40 +0200

    add nodemailer + verify smtp connexion

--- api/src/config/sequelize-config.js
+++ api/src/config/sequelize-config.js
@@ -1,10 +1,16 @@
 import { Sequelize } from "sequelize";
 import dotenv from "dotenv";
+import fs from "fs";
 
-dotenv.config();
+let envFile = ".env";
+if (fs.existsSync(".env.local")) {
+	envFile = ".env.local";
+}
+
+dotenv.config({ path: envFile });
 
 const sequelize = new Sequelize(process.env.POSTGRES_URI, {
-    logging: false, // Désactive les logs Sequelize
+	logging: false, // Désactive les logs Sequelize
 });
 
 sequelize.sync();
