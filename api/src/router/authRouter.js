2efae739547f85faf7f96cac7ed9d2f4dccee72b
Rafael93150 <tavaresrafael93@gmail.com>
2023-09-22 15:38:24 +0200

    add validateEmail route

--- api/src/router/authRouter.js
+++ api/src/router/authRouter.js
@@ -1,5 +1,10 @@
 import express from "express";
-import { register, login, logout } from "../routes/authRoutes.js";
+import {
+    register,
+    login,
+    logout,
+    validateEmail,
+} from "../routes/authRoutes.js";
 import authMiddleware from "../middlewares/authMiddleware.js";
 
 const router = express.Router();
@@ -7,5 +12,6 @@ const router = express.Router();
 router.post("/register", register);
 router.post("/login", login);
 router.post("/logout", authMiddleware, logout);
+router.post("/validate", validateEmail);
 
 export default router;
--- api/src/routes/authRoutes.js
+++ api/src/routes/authRoutes.js
@@ -1,6 +1,7 @@
 import User from "../models/postgres-user.js";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
+import { generateAuthentificationToken } from "../services/auth.service.js";
 
 export const register = async (req, res) => {
     try {
@@ -11,20 +12,25 @@ export const register = async (req, res) => {
 
         const hashedPassword = await bcrypt.hash(password, 10);
 
-        const existingUser = await User.findOne({ email });
-        if (existingUser)
-            throw new Error(`L'adresse email ${email} est déjà utilisée`);
+        const existingUser = await User.findOne({
+            where: { email },
+        });
 
-        const user = new User({
+        if (existingUser) throw new Error(`Email "${email}" is already taken`);
+
+        const authentificationToken = generateAuthentificationToken();
+
+        const newUser = await User.create({
             firstname,
             lastname,
             email,
             password: hashedPassword,
             role,
+            authentificationToken,
         });
 
         const payload = {
-            userId: user._id,
+            userId: newUser.id,
         };
 
         const options = {
@@ -33,15 +39,13 @@ export const register = async (req, res) => {
 
         const token = jwt.sign(payload, process.env.SECRET_KEY, options);
 
-        await user.save();
-
         res.json({
-            message: "Utilisateur créé avec succès",
+            message: "User created successfully",
             token: token,
         });
     } catch (error) {
         res.status(500).json({
-            error: `Une erreur est survenue lors de la création de l'utilisateur : ${error}`,
+            error: `An error occurred while creating the user : ${error}`,
         });
     }
 };
@@ -50,20 +54,20 @@ export const login = async (req, res) => {
     try {
         const { email, password } = req.body;
 
-        const user = await User.findOne({ email });
+        const user = await User.findOne({ where: { email } });
 
         if (!user) {
-            return res.status(401).json({ message: "Identifiants invalides" });
+            return res.status(401).json({ message: "Invalid credentials" });
         }
 
         const isPasswordValid = await bcrypt.compare(password, user.password);
 
         if (!isPasswordValid) {
-            return res.status(401).json({ message: "Identifiants invalides" });
+            return res.status(401).json({ message: "Invalid credentials" });
         }
 
         const payload = {
-            userId: user._id,
+            userId: user.id,
         };
 
         const options = {
@@ -73,7 +77,10 @@ export const login = async (req, res) => {
         const token = jwt.sign(payload, process.env.SECRET_KEY, options);
 
         const userWithToken = {
-            ...user.toObject(),
+            firstname: user.firstname,
+            lastname: user.lastname,
+            email: user.email,
+            authentificationToken: user.authentificationToken,
             token,
         };
 
@@ -81,8 +88,37 @@ export const login = async (req, res) => {
 
         res.json(userWithToken);
     } catch (error) {
+        console.error("Error during login:", error);
         res.status(500).json({
-            error: `Une erreur est survenue lors de la connexion : ${error}`,
+            error: `An error occurred during login: ${error.message}`,
+        });
+    }
+};
+
+export const validateEmail = async (req, res) => {
+    try {
+        const { email, authentificationToken } = req.body;
+
+        const user = await User.findOne({
+            where: { email },
+        });
+
+        if (!user) throw new Error(`Email "${email}" not found`);
+
+        if (user.authentificationToken !== authentificationToken)
+            throw new Error("Invalid token");
+
+        await User.update(
+            { isValidate: true, authentificationToken: null },
+            { where: { email } }
+        );
+
+        res.json({
+            message: "Email validated successfully",
+        });
+    } catch (error) {
+        res.status(500).json({
+            error: `An error occurred while validating the email : ${error}`,
         });
     }
 };
