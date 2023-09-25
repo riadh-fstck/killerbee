0efebb0f883fdf1683f21cac02c4697820339f5f
riadh-fstck <r.benchouche1@gmail.com>
2023-09-25 12:04:30 +0200

    init client front dev

---
+++ client/src/layout/LayoutComponent.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import NavBar from "./NavBar.vue";
+import FooterBar from "./FooterBar.vue";
+</script>
+<template>
+  <NavBar/>
+  <main>
+    <slot/>
+  </main>
+  <FooterBar/>
+</template>
